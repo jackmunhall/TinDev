6b2c249eff1083de948d4a5bae31cfc70fe506b9
Jack Munhall <jman777jman@gmail.com>
2022-12-07 15:17:46 -0500

    everything but recruiter create and view

--- src/tindev/forms.py
+++ src/tindev/forms.py
@@ -1,10 +1,23 @@
 from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, EmailInput
 from .models import *
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'type', 'city', 'state', 'skills', 'description', 'expiration', 'status']
+        widgets = {
+            'title': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Name'
+                }),
+            'email': EmailInput(attrs={
+                'class': "form-control", 
+                'style': 'max-width: 300px;',
+                'placeholder': 'Email'
+                })
+        }
 
 class UserForm(ModelForm):
     class Meta:
